1432ca23c4631004ec771b5adb798fc698502be1
weyyam <williammichaelmcmahon@gmail.com>
2023-09-11 16:56:51 -0400

    fixed nav issues controller works now

--- AndroidStudioProjects/TierFood/app/src/main/java/com/weyyam/tierfood/MainActivity.kt
+++ AndroidStudioProjects/TierFood/app/src/main/java/com/weyyam/tierfood/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.weyyam.tierfood.ui.theme.TierFoodTheme
